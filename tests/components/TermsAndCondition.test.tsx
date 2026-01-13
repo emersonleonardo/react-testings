126414851660bf4e35316c4d8007320e57d76406
Leonardo Lima <leonardodelima96@hotmail.com.br>
2026-01-12 21:28:04 -0300

    Testes: Otimizando os testes do componente TermsAndCondition

--- tests/components/TermsAndCondition.test.tsx
+++ tests/components/TermsAndCondition.test.tsx
@@ -3,31 +3,31 @@ import TermsAndConditions from "../../src/components/TermsAndConditions";
 import userEvent from "@testing-library/user-event";
 
 describe("TermsAndCondition", () => {
-  it("Deve renderizar com o texto correto e estado inicial", () => {
+  const renderComponents = () => {
     render(<TermsAndConditions />);
 
-    const heading = screen.getByRole("heading");
-    expect(heading).toBeInTheDocument();
+    return {
+      heading: screen.getByRole("heading"),
+      checkbox: screen.getByRole("checkbox"),
+      button: screen.getByRole("button"),
+      buttonSubmit: screen.getByRole("button", {
+        name: /submit/i,
+      }),
+    };
+  };
+  it("Deve renderizar com o texto correto e estado inicial", () => {
+    const { heading, checkbox, buttonSubmit } = renderComponents();
+
     expect(heading).toHaveTextContent("Terms & Conditions");
-
-    const checkbox = screen.getByRole("checkbox");
-    expect(checkbox).toBeInTheDocument();
     expect(checkbox).not.toBeChecked();
-
-    const button = screen.getByRole("button", {
-      name: /submit/i,
-    });
-    expect(button).toBeInTheDocument();
-    expect(button).toBeDisabled();
+    expect(buttonSubmit).toBeDisabled();
   });
 
   it("Deve habilitar o botão quando o checkbox estiver marcado", async () => {
-    render(<TermsAndConditions />);
+    const { checkbox, button } = renderComponents();
 
-    const checkbox = screen.getByRole("checkbox");
     const user = userEvent.setup();
-
     await user.click(checkbox);
-    expect(screen.getByRole("button")).toBeEnabled();
+    expect(button).toBeEnabled();
   });
 });
